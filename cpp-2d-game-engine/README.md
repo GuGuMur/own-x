e75071ad03a45e278c65c77bbff8a399b841c3cd
GuGuMur <2221533105@qq.com>
2025-12-05 20:12:01 +0800

    [mini2d] feat: uncompress zip

--- cpp-2d-game-engine/README.md
+++ cpp-2d-game-engine/README.md
@@ -8,6 +8,8 @@ sudo apt-get install libasound2-dev libpulse-dev libaudio-dev libx11-dev libxext
 glad.dav1d.de
 
 git submodule add https://github.com/nothings/stb.git include/stb
+~git submodule add https://github.com/richgel999/miniz.git include/miniz~
+using the release version of miniz instead and copy to src
 
 # 初始化并更新子模块
 git submodule update --init --recursive
--- cpp-2d-game-engine/src/main.cpp
+++ cpp-2d-game-engine/src/main.cpp
@@ -6,22 +6,49 @@
 #include <stb/stb_truetype.h>
 // #define STB_VORBIS_HEADER_ONLY
 #include <stb/stb_vorbis.c>
+#include <miniz.h>
 
 static int winW = 640;
 static int winH = 480;
 
 namespace {
+    class Zip {
+        mz_zip_archive zip{};
+    public:
+        Zip(const char* name) {
+            memset(&zip, 0, sizeof(zip));
+            mz_bool ret = mz_zip_reader_init_file(&zip, name, 0);
+            if (ret == MZ_FALSE) {
+                printf("file not found: %s\n", name);
+                exit(-1);
+            }
+        }
+        ~Zip() {
+            mz_zip_reader_end(&zip);
+        }
+
+        void* open(const char* name, size_t & size) {
+            return mz_zip_reader_extract_file_to_heap(&zip, name, &size, 0);
+        }
+
+        void close(void* p) {
+            mz_free(p);
+        }
+
+    };
+    Zip* gZip;
     class Shader {
         GLuint vsID;
         GLuint fsID;
         GLuint programID;
+
     public:
-        Shader(const char* vsSrc, const char* fsSrc) {
+        Shader(const char *vsSrc, const char *fsSrc) {
             auto checkShaderInfo = [](const GLuint id) {
                 GLint len;
                 glGetShaderiv(id, GL_INFO_LOG_LENGTH, &len);
                 if (len != 0) {
-                    std::vector<char> info(len+1);
+                    std::vector<char> info(len + 1);
                     glGetShaderInfoLog(id, len, nullptr, info.data());
                     info[len] = *"\0";
                     printf("%s\n", info.data());
@@ -31,7 +58,7 @@ namespace {
                 GLint len;
                 glGetProgramiv(id, GL_INFO_LOG_LENGTH, &len);
                 if (len != 0) {
-                    std::vector<char> info(len+1);
+                    std::vector<char> info(len + 1);
                     glGetProgramInfoLog(id, len, nullptr, info.data());
                     info[len] = *"\0";
                     printf("%s\n", info.data());
@@ -53,37 +80,48 @@ namespace {
             glLinkProgram(programID);
             checkProgramInfo(programID);
         }
-        [[nodiscard]] GLuint getID () const { return programID;}
-        void attrib(const char* name, const GLint size, const GLenum type, const GLsizei stride = 0, const void* pointer = nullptr) const{
+
+        [[nodiscard]] GLuint getID() const { return programID; }
+
+        void attrib(const char *name, const GLint size, const GLenum type, const GLsizei stride = 0,
+                    const void *pointer = nullptr) const {
             const GLint location = glGetAttribLocation(programID, name);
             glEnableVertexAttribArray(location);
             glVertexAttribPointer(location, size, type, GL_FALSE, stride, pointer);
         }
-        void setVec4(const char* name, const float v0, const float v1, const float v2, const float v3) const { // x, y, z, w
+
+        void setVec4(const char *name, const float v0, const float v1, const float v2, const float v3) const {
+            // x, y, z, w
             const GLint location = glGetUniformLocation(programID, name);
             glUniform4f(location, v0, v1, v2, v3);
         }
-        void setTexture(const char* name, const GLint texture) const {
+
+        void setTexture(const char *name, const GLint texture) const {
             const GLint location = glGetUniformLocation(programID, name);
             glUniform1i(location, texture);
         }
+
         void use() const {
             glUseProgram(programID);
         }
+
         ~Shader() {
             glDeleteShader(vsID);
             glDeleteShader(fsID);
             glDeleteProgram(programID);
         }
     };
+
     class Buffer {
         GLuint bufferID{};
+
         void makeBuffer(const std::vector<float> &point) {
             glGenBuffers(1, &bufferID);
             glBindBuffer(GL_ARRAY_BUFFER, bufferID);
             glBufferData(GL_ARRAY_BUFFER, point.size() * sizeof(float), point.data(), GL_STATIC_DRAW);
             glBindBuffer(GL_ARRAY_BUFFER, 0);
         };
+
     public:
         Buffer(float x, float y, float w, float h, bool uv = true) {
             // uv: use texture
@@ -105,29 +143,44 @@ namespace {
             };
             makeBuffer(uv ? rectUV : rect);
         }
+
         explicit Buffer(const std::vector<float> &point) {
             makeBuffer(point);
         }
+
         [[nodiscard]] GLuint getID() const { return bufferID; }
+
         void bind() const {
             glBindBuffer(GL_ARRAY_BUFFER, bufferID);
         }
-        void unbind() const {
+
+        static void unbind() {
             glBindBuffer(GL_ARRAY_BUFFER, 0);
         }
+
         ~Buffer() {
             glDeleteBuffers(1, &bufferID);
         }
     };
+
     class Texture {
         GLuint textureID;
+
     public:
-        Texture(const char* name) {
+        Texture(const char *name) {
             int w, h, c;
-            stbi_uc* p = stbi_load(name, &w, &h, &c, 4); // r g b a 四个通道
+            stbi_uc *p = stbi_load(name, &w, &h, &c, 4); // r g b a 四个通道
             if (p == nullptr) {
-                printf("[ERROR] failed to load %s\n", name);
-                exit(-1);
+                size_t size;
+                void* data = gZip->open(name, size);
+                if (data == nullptr) {
+                    printf("[ERROR] failed to load %s\n", name);
+                    exit(-1);
+                }
+                else {
+                    p = stbi_load_from_memory(reinterpret_cast<stbi_uc*>(data), static_cast<int>(size), &w, &h, &c, 4);
+                    gZip->close(data);
+                }
             }
             glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
             glGenTextures(1, &textureID);
@@ -139,7 +192,8 @@ namespace {
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
             stbi_image_free(p);
         }
-        Texture (const std::vector<unsigned char>&bitmap, int w, int h) {
+
+        Texture(const std::vector<unsigned char> &bitmap, int w, int h) {
             glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
             glGenTextures(1, &textureID);
             glBindTexture(GL_TEXTURE_2D, textureID);
@@ -149,38 +203,56 @@ namespace {
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         }
+
         [[nodiscard]] GLuint getID() const { return textureID; }
-        void bind (GLint texture) const {
+
+        void bind(GLint texture) const {
             glActiveTexture(GL_TEXTURE0 + texture);
             glBindTexture(GL_TEXTURE_2D, textureID);
         }
-        void unbind () const {
+
+        void unbind() const {
             glBindTexture(GL_TEXTURE_2D, 0);
         }
+
         ~Texture() {
             glDeleteTextures(1, &textureID);
         }
     };
+
     class Font {
         std::vector<unsigned char> font;
-        stbtt_fontinfo info;
+        stbtt_fontinfo info{};
+
     public:
-        Font(const char* name) {
-            FILE* f = fopen(name, "rb");
+        explicit Font(const char *name) {
+            FILE *f = fopen(name, "rb");
             if (f == nullptr) {
-                printf("failed to open font file: %s\n", name);
-                exit(-1);
+                size_t size;
+                void* data = gZip->open(name, size);
+                if (data == nullptr) {
+                    printf("failed to open font file: %s\n", name);
+                    exit(-1);
+                }
+                font.resize(size);
+                memcpy(font.data(), data, size);
+                gZip->close(data);
             }
+            else {
+
             fseek(f, 0, SEEK_END);
             long size = ftell(f);
             fseek(f, 0, SEEK_SET);
             font.resize(size);
             fread(&font[0], 1, size, f);
             fclose(f);
+            }
 
             stbtt_InitFont(&info, font.data(), 0);
         }
-        void makeBitmap(wchar_t code, float size, std::vector<unsigned char>& bitmap, int& x0, int& y0, int& w, int& h) {
+
+        void makeBitmap(wchar_t code, float size, std::vector<unsigned char> &bitmap, int &x0, int &y0, int &w,
+                        int &h) const {
             float scale = stbtt_ScaleForPixelHeight(&info, size);
             int x1, y1;
             stbtt_GetCodepointBitmapBox(&info, code, scale, scale, &x0, &y0, &x1, &y1);
@@ -190,9 +262,29 @@ namespace {
             stbtt_MakeCodepointBitmap(&info, bitmap.data(), w, h, w, scale, scale, code);
         }
     };
+
     class Audio {
-            SDL_AudioDeviceID audioDeviceID;
-            stb_vorbis* vorbis = nullptr;
+        struct Vorbis {
+            stb_vorbis *vorbis = nullptr;
+            int loop = 0;
+            int pause = 0;
+            void* data = nullptr;
+        };
+
+        static constexpr int MAX_AUDIO = 5;
+        SDL_AudioDeviceID audioDeviceID;
+        Vorbis vorbis[MAX_AUDIO];
+        std::vector<short> samples[MAX_AUDIO];
+
+        int findVorbis() const {
+            for (int i = 0; i < MAX_AUDIO; ++i) {
+                if (vorbis[i].pause == 0) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
     public:
         Audio() {
             SDL_AudioSpec spec;
@@ -204,44 +296,94 @@ namespace {
             spec.userdata = nullptr;
             audioDeviceID = SDL_OpenAudioDevice(nullptr, 0, &spec, nullptr, 0);
         }
+
         ~Audio() {
             SDL_PauseAudioDevice(audioDeviceID, 1);
             SDL_CloseAudioDevice(audioDeviceID);
-            stb_vorbis_close(vorbis);
-        }
-
-        void open(const char* name) {
-            int error = 0;
-            vorbis = stb_vorbis_open_filename(name, &error, nullptr);
-            if (vorbis == nullptr) {
-                printf("failed to open file: %s\n", name);
-                exit(-1);
+            for (const auto &item : vorbis) {
+                stb_vorbis_close(item.vorbis);
+                gZip->close(item.data);
             }
-
-            SDL_PauseAudioDevice(audioDeviceID, 0);
         }
 
+        int open(const char *name, const int loop = 1) {
+            int idx = findVorbis();
+            if (idx == -1) {
+                return -1;
+            }
+            int error = 0;
+            vorbis[idx].vorbis = stb_vorbis_open_filename(name, &error, nullptr);
+            if (vorbis[idx].vorbis == nullptr) {
+                size_t size;
+                void* data = gZip->open(name, size);
+                if (data == nullptr) {
+                    printf("failed to open file: %s\n", name);
+                    exit(-1);
+                }
+                vorbis[idx].data = data;
+                vorbis[idx].vorbis = stb_vorbis_open_memory(reinterpret_cast<const unsigned char*>(data), static_cast<int>(size), &error, nullptr);
+            }
+            vorbis[idx].loop = loop;
+            vorbis[idx].pause = 0;
+            return idx;
+            // SDL_PauseAudioDevice(audioDeviceID, 0);
+        }
+        void close(const int idx) {
+            stb_vorbis_close(vorbis[idx].vorbis);
+            gZip->close(vorbis[idx].data);
+            vorbis[idx] = Vorbis();
+        }
         void play() {
             Uint32 queuedAudioSize = SDL_GetQueuedAudioSize(audioDeviceID);
             if (queuedAudioSize == 0) {
-                std::vector<short> samples(2048);
-                int ret = stb_vorbis_get_samples_short_interleaved(vorbis, 2, samples.data(), static_cast<int>(samples.size()));
-                if (ret == 0) {
-                    stb_vorbis_seek_start(vorbis);
+                for (int i = 0; i < MAX_AUDIO; ++i) {
+                    if (!vorbis[i].vorbis || vorbis[i].pause) {
+                        continue;
+                    }
+                    samples[i].resize(2048);
+                    int ret = stb_vorbis_get_samples_short_interleaved(vorbis[i].vorbis, 2, samples[i].data(),
+                                                                       static_cast<int>(samples[i].size()));
+                    if (ret == 0) {
+                        stb_vorbis_seek_start(vorbis[i].vorbis);
+                        if (!vorbis[i].loop) {
+                            vorbis[i].pause = 1;
+                        }
+                    }
                 }
-                SDL_QueueAudio(audioDeviceID, samples.data(), static_cast<Uint32>(samples.size()) * sizeof(short));
+                std::vector<short> samples_mix(2048);
+                for (size_t k = 0; k < samples_mix.size(); ++k) {
+                    int sample = 0;
+                    for (int i = 0; i < MAX_AUDIO; ++i) {
+                        if (!vorbis[i].vorbis || vorbis[i].pause) {
+                            continue;
+                        }
+                        sample += samples[i][k];
+                    }
+                    constexpr int s16max = static_cast<short>(0x7FFF);
+                    constexpr int s16min = static_cast<short>(0x8000);
+                    if (sample > s16max) {
+                        sample = s16max;
+                    }
+                    if (sample < s16min) {
+                        sample = s16min;
+                    }
+                    samples_mix[k] = static_cast<short>(sample);
+                }
+                SDL_QueueAudio(audioDeviceID, samples_mix.data(), static_cast<Uint32>(samples_mix.size()) * sizeof(short));
             }
         }
 
-        void pause(int pause) {
+        void pause(int pause) const {
             SDL_PauseAudioDevice(audioDeviceID, pause);
         }
     };
 }
-int main(int argc, char** argv) {
+
+int main(int argc, char **argv) {
     printf("mini2d\n");
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
-    SDL_Window* window = SDL_CreateWindow("mini2d", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, winW, winH, SDL_WINDOW_OPENGL);
+    SDL_Window *window = SDL_CreateWindow("mini2d", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, winW, winH,
+                                          SDL_WINDOW_OPENGL);
     const SDL_GLContext context = SDL_GL_CreateContext(window);
     SDL_GL_MakeCurrent(window, context);
     if (!gladLoadGLLoader(SDL_GL_GetProcAddress)) {
@@ -251,13 +393,15 @@ int main(int argc, char** argv) {
 
     printf("GL_VERSION:%s\n", reinterpret_cast<const char *>(glGetString(GL_VERSION)));
 
-    auto _checkGLError = [](const char* file, const int line) {
+    gZip = new Zip("../data/data.zip");
+
+    auto _checkGLError = [](const char *file, const int line) {
         for (GLint error = glGetError(); error != GL_NO_ERROR; error = glGetError()) {
             printf("[%s][%d]: 0x%04x\n", file, line, error);
         }
     };
 
-    #define checkGLError() _checkGLError(__FILE__, __LINE__)
+#define checkGLError() _checkGLError(__FILE__, __LINE__)
 
     const auto vsSrc = R"(
         #version 330 core
@@ -342,7 +486,7 @@ int main(int argc, char** argv) {
         glClear(GL_COLOR_BUFFER_BIT);
         glViewport(0, 0, winW, winH);
 
-        auto drawPoint = [&](const Buffer& buffer, const Shader& shader) {
+        auto drawPoint = [&](const Buffer &buffer, const Shader &shader) {
             buffer.bind();
             shader.attrib("position", 2, GL_FLOAT);
             shader.use();
@@ -350,7 +494,7 @@ int main(int argc, char** argv) {
             glDrawArrays(GL_POINTS, 0, 2);
             buffer.unbind();
         };
-        auto drawLine = [&](const Buffer& buffer, const Shader& shader) {
+        auto drawLine = [&](const Buffer &buffer, const Shader &shader) {
             buffer.bind();
             shader.attrib("position", 2, GL_FLOAT);
             shader.use();
@@ -358,19 +502,19 @@ int main(int argc, char** argv) {
             glDrawArrays(GL_LINES, 0, 4);
             buffer.unbind();
         };
-        auto drawRect = [&](const Buffer& buffer, const Shader& shader) {
+        auto drawRect = [&](const Buffer &buffer, const Shader &shader) {
             buffer.bind();
             shader.attrib("position", 2, GL_FLOAT);
             shader.use();
             shader.setVec4("color", 0.0f, 0.0f, 1.0f, 1.0f);
             glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
             buffer.unbind();
-
         };
-        auto drawRectUV = [&](const Buffer& buffer, const Shader& shader, const Texture& texture) {
+        auto drawRectUV = [&](const Buffer &buffer, const Shader &shader, const Texture &texture) {
             buffer.bind();
             shader.attrib("position", 2, GL_FLOAT, 4 * sizeof(float), nullptr);
-            shader.attrib("texcoord", 2, GL_FLOAT, 4 * sizeof(float), reinterpret_cast<const void *>(2 * sizeof(float)));
+            shader.attrib("texcoord", 2, GL_FLOAT, 4 * sizeof(float),
+                          reinterpret_cast<const void *>(2 * sizeof(float)));
             shader.use();
             shader.setVec4("color", 1.0f, 1.0f, 1.0f, 1.0f);
             shader.setTexture("texture0", 0);
@@ -392,6 +536,7 @@ int main(int argc, char** argv) {
     };
     Audio audio;
     audio.open("../data/MeetingTheStars.ogg");
+    audio.open("../data/SadSoul.ogg");
     audio.pause(0);
 
     int done = 0;
@@ -406,5 +551,6 @@ int main(int argc, char** argv) {
         audio.play();
     };
     SDL_DestroyWindow(window);
+    delete gZip;
     return 0;
 }
